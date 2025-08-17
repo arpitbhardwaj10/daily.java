091bd5961ab4bf6c86ab2de94212bd2bf6c52d2b
arpitbhardwaj10 <104090555+arpitbhardwaj10@users.noreply.github.com>
2025-08-18 04:09:54 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,24 @@
+package Operators;
+
+public class Example {
+    public static void main(String[]args){
+        byte a = 24;
+        byte b = 23;
+        int c = a-b;
+        System.out.println(c);
+
+        double i = 24.4;
+        int j = 24;
+        double k = i-j;
+        System.out.println(k);
+
+        char x ='A';
+        char y = 'Z';
+        int z = x-y;
+        System.out.println(z);
+
+
+
+    }
+    
+}
